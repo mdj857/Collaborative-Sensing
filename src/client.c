10d3ac4f754843719916dcf2ad52e4b908ff9cef
TerryEunice <pcreipold@utexas.edu>
2018-10-01 14:36:02 -0500

    Multithreading the server and client

--- src/client.c
+++ src/client.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -8,26 +9,83 @@
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sched.h>
+#include <unistd.h>
+
 
 #define BUFF_SIZE 256
+#define STACK_SIZE 2048
+#define FALSE 0
+#define TRUE 1
 
-int main(void)
-{
-  int sockfd = 0,n = 0;
-  int done = 0;
-  char recvBuff[1024];
-  char sendBUFF[BUFF_SIZE];
-  char empty[BUFF_SIZE];
-  struct sockaddr_in serv_addr;
- 
-  //setup empty
-  int i =0;
-  for(;i < BUFF_SIZE; i++){
-     empty[i] = '\0';
+
+int sockfd=0;
+
+//thread to read from the fifo and send out the message
+int transmit_fifo(void *sparkle){
+  int nr;
+  char read_buff[BUFF_SIZE];
+   int fifo2 = open("cli_transmit", O_RDONLY); //open fifo for reading
+   if(fifo2 == -1){
+      printf("error opening fifo for reading, %d", errno);
+   }
+   while(1){
+      //read and send to server
+      nr = read(fifo2, read_buff, sizeof(read_buff)-1);
+      read_buff[nr] = 0; //put termination at end of string
+      puts("Transmit FiFo read. Sending to Server.");
+      write(sockfd, read_buff, strlen(read_buff));
+     
+      if(!strcmp("done\n", read_buff)){
+        //we are done so send the message and then close down
+        puts("Trixie says the client is shutting down.");
+        close(sockfd); //close the current request
+        exit(0); //will this work lol
+      }
+
+ }
+  puts("shouldn't go here");
+  return(0);
+}
+
+int recieve_fifo(void *unused){
+
+  char recvBuff[BUFF_SIZE];
+
+  while(1){
+    //monitor network and print recieved message to screen
+    //TODO: replace print with FiFo write
+    int num_char = read(sockfd, recvBuff, sizeof(recvBuff));
+    recvBuff[num_char] = '\0'; //must add null termination
+    fputs(recvBuff, stdout);
+    puts(" ");
+    //client cannot yet be shutdown over the network...
+    //if(!strcmp("done\n", recvBuff)){
+      //client tells us that we are done so close connexion
+     // puts("Rarity says the sever is closing down");
+      //close(sockfd); //close the current request
+      //exit(0); //will this work lol
+   // }
   }
+  return(0);
+}
 
 
-  memset(recvBuff, '0' ,sizeof(recvBuff));
+int main(void){
+  int fifo;
+  int n = 0;
+  char user_input[BUFF_SIZE];  //user input string 
+  struct sockaddr_in serv_addr;
+  char *stack, *stack_top,*stack2;
+  int done = FALSE;
+  int numrv;
+  int num_char=0;
+
+
   if((sockfd = socket(AF_INET, SOCK_STREAM, 0))< 0)
     {
       printf("\n Error : Could not create socket \n");
@@ -44,29 +102,55 @@ int main(void)
       return 1;
     }
 
+//Transmit FiFo Setup code
+  //create fifo, with all permisssions for everyone
+  int res = mkfifo("cli_transmit", S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+  if(res && (errno != EEXIST)){ //OK if FiFo already exists
+    puts("Error creating the FiFo. Program will now termiate.\n");
+    exit(1);
+  }
+  //open for writing
+  fifo = open("cli_transmit", O_RDWR); //open the fifo for reading and writing 
+  if(fifo == -1){
+    printf("Fifo failed to open, errno %d\n", errno);
+    exit(2);
+  }
+
+   //launch the sending thread
+   stack = malloc(STACK_SIZE);
+  if(stack == NULL){
+     exit(3);;
+  }
+  stack_top = stack + STACK_SIZE;  /* Assume stack grows toward zero */
+  int new_thrd = clone(&transmit_fifo, stack_top,CLONE_THREAD|CLONE_SIGHAND|CLONE_VM, stack);
+    if(new_thrd == -1){
+    puts("Recieve thread could not be created...Sorry Twilight... :(");
+    printf("errno %d\n", errno);
+    exit(2);
+  }
+
+  //launch the recieving thread
+  stack2 = malloc(STACK_SIZE);
+  if(stack2 == NULL){
+     exit(3);;
+  }
+  stack_top = stack2 + STACK_SIZE;  /* Assume stack grows toward zero */
+  int nw_thrd2 = clone(&recieve_fifo, stack_top, CLONE_THREAD|CLONE_SIGHAND|CLONE_VM, stack);
+    if(nw_thrd2 == -1){
+    puts("Thread could not be created...Sorry Twilight... :(");
+    printf("errno %d\n", errno);
+    exit(2);
+  }
+
+
    while(!done){
-        printf("Enter message to send back to server:\n");	   
-	strcpy(sendBUFF, empty);
-	fgets(sendBUFF, BUFF_SIZE, stdin);
-	if(!strcmp(sendBUFF, "done\n")){
-	   //said we are done so lets quit
-	  // puts("Done Sending!"); 
-		done = 1;
-	}
-        //send data to the server
-	//printf("To server: %s", sendBUFF);
-        write(sockfd, sendBUFF, strlen(sendBUFF)); 
-	
-	//recieve data from server and print
-	n = read(sockfd, recvBuff, sizeof(recvBuff)-1);
-        if( n < 0){
-           printf("\n Read Error \n");
-        }    
-        recvBuff[n] = 0;
-        if(fputs(recvBuff, stdout) == EOF){
-         printf("\n Error : Fputs error");
-        }
-        printf("\n");
+     //get user input for message
+     //sleep(1);
+     puts("Enter message to send between threads");
+     fgets(user_input, BUFF_SIZE, stdin);
+     //put it in the FiFo 
+     write(fifo, user_input, strlen(user_input));
+     //rinse and repeat.
     }
 
   return 0;
--- src/server.c
+++ src/server.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -7,39 +8,92 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sched.h>
+#include <unistd.h>
 
 
 #define BUFF_SIZE 256
+#define STACK_SIZE 2048
+#define FALSE 0
+#define TRUE 1
 
-int main(void)
-{
-  int listenfd = 0,connfd = 0;
-  int contin = 1; 
-  struct sockaddr_in serv_addr;
- 
-  char sendBuff[1025];  
+
+int connfd=0;
+
+//thread to read from the fifo and send out the message
+int transmit_fifo(void *sparkle){
+  int nr;
+  char read_buff[BUFF_SIZE];
+   int fifo2 = open("srv_transmit", O_RDONLY); //open fifo for reading
+   if(fifo2 == -1){
+      printf("error opening fifo for reading, %d", errno);
+   }
+   while(1){
+      //read and send to client
+      nr = read(fifo2, read_buff, sizeof(read_buff)-1);
+      read_buff[nr] = 0; //put termination at end of string
+      puts("Transmit FiFo read. Sending to client.");
+      write(connfd, read_buff, strlen(read_buff));
+ }
+  puts("shouldn't go here");
+  return(0);
+}
+
+int recieve_fifo(void *unused){
+
   char recvBuff[BUFF_SIZE];
-  char empty[BUFF_SIZE];
-  char resp_to_cly[BUFF_SIZE];
+
+  while(1){
+    //monitor network and print recieved message to screen
+    //TODO: replace print with FiFo write
+    int num_char = read(connfd, recvBuff, sizeof(recvBuff));
+    recvBuff[num_char] = '\0'; //must add null termination
+    fputs(recvBuff, stdout);
+    puts(" ");
+
+    if(!strcmp("done\n", recvBuff)){
+      //client tells us that we are done so close connexion
+      puts("Rarity says the sever is closing down");
+      close(connfd); //close the current request
+      exit(0); //will this work lol
+    }   
+  }
+  return(0);
+}
+
+int main(void){
+  int fifo = 0;
+  int listenfd = 0;
+  int contin = 1; 
+  char user_input[BUFF_SIZE];  //user input string 
+  struct sockaddr_in serv_addr;
+  char *stack, *stack_top,*stack2;
+  int done = FALSE;
   int numrv;  
   int num_char=0; 
-  //initialize empty to all zeros
-  int i;
-  for(i =0; i < BUFF_SIZE; i++){
-     empty[i]=0;
-  }
   
-  //allow user to set up server response
-  puts("Enter server response message:");
-  fgets(resp_to_cly, BUFF_SIZE, stdin);
-
+  //Transmit FiFo Setup code
+  //create fifo, with all permisssions for everyone
+  int res = mkfifo("srv_transmit", S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+  if(res && (errno != EEXIST)){ //OK if FiFo already exists
+    puts("Error creating the FiFo. Program will now termiate.\n");
+    exit(1);
+  }
+  //open for writing
+  fifo = open("srv_transmit", O_RDWR); //open the fifo for reading and writing 
+  if(fifo == -1){
+    printf("Fifo failed to open, errno %d\n", errno);
+    exit(2);
+  }
 
   //server set up code
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
   printf("socket retrieve success\n");
   
   memset(&serv_addr, '0', sizeof(serv_addr));
-  memset(sendBuff, '0', sizeof(sendBuff));
       
   serv_addr.sin_family = AF_INET;    
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
@@ -51,44 +105,47 @@ int main(void)
       printf("Failed to listen\n");
       return -1;
   }
-
-
-  
   connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL); // accept awaiting request.
   
-  while(contin)
-    { 
-      //clear recvBuff
-      //strcpy(recvBuff, empty);
-      //server reads data from the client and displays it	    
-      num_char = read(connfd, recvBuff, sizeof(recvBuff));
-      recvBuff[num_char] = '\0'; //must add null termination
-      fputs(recvBuff, stdout);
-      puts(" ");
+  //launch the sending thread
+   stack = malloc(STACK_SIZE);
+  if(stack == NULL){
+     exit(3);;
+  }
+  stack_top = stack + STACK_SIZE;  /* Assume stack grows toward zero */
+  int new_thrd = clone(&transmit_fifo, stack_top,CLONE_THREAD|CLONE_SIGHAND|CLONE_VM, stack);
+    if(new_thrd == -1){
+    puts("Recieve thread could not be created...Sorry Twilight... :(");
+    printf("errno %d\n", errno);
+    exit(2);
+  }
 
-      if(!strcmp("done", recvBuff)){
-         //client tells us that we are done so close connexion
-	 contin = 0;
-	 memset(resp_to_cly, '\0', sizeof(char)*BUFF_SIZE);
-	 strcpy(resp_to_cly, "Connexion Terminated. :)"); 
-      }
+  //launch the recieving thread  
+  stack2 = malloc(STACK_SIZE);
+  if(stack2 == NULL){
+     exit(3);;
+  }
+  stack_top = stack2 + STACK_SIZE;  /* Assume stack grows toward zero */
+  int nw_thrd2 = clone(&recieve_fifo, stack_top, CLONE_THREAD|CLONE_SIGHAND|CLONE_VM, stack);
+    if(nw_thrd2 == -1){
+    puts("Thread could not be created...Sorry Twilight... :(");
+    printf("errno %d\n", errno);
+    exit(2);
+  }  
+  
+  //this code will write into the FIFO
+  //TODO replace with infinite loop
+  while(contin){ 
+     //get user input for message
+     sleep(1);
+     puts("Enter message to send between threads");
+     fgets(user_input, BUFF_SIZE, stdin);
+     //put it in the FiFo 
+     write(fifo, user_input, strlen(user_input));
+     //rinse and repeat.
+ 
+  }   
 
-      //send data back to the client
-      //strcpy(sendBuff, "Luna thanks you for your input!");
-      write(connfd, resp_to_cly, strlen(resp_to_cly));
-
-      //sleep(1);
-      //Test retreiving data from the client to the server
-      
-      //puts("Do you want to quit the server? Y/N\n");
-      //char reply = 0;
-      //reply = getchar();
-      //if(reply == 'y' || reply == 'Y'){
-      //	   contin = 0;
-      //}
-
-    } 
-
-  close(connfd); //close the current request. Moved to the end so that we can test stuff    
   return 0;
+
 }
