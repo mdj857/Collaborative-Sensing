26ebc9dc4a67abad83be549d7a750e0ee7af20cd
phucdang18 <phucdang18@utexas.edu>
2018-10-10 16:32:03 -0500

    ekf modified

--- src/EKF.py
+++ src/EKF.py
@@ -3,7 +3,7 @@ from filterpy.common import Q_discrete_white_noise
 from filterpy.kalman import ExtendedKalmanFilter
 from numpy import eye, array, asarray
 import numpy as np
-import src.planetDectector as cc
+from src.planetDetector import *
 
 constant = 1
 
@@ -12,7 +12,7 @@ def HJacobian_at(x):
 
     angular_vel = x[0]
     angular_pos = x[1]
-    return array([[0, -sin(angular_pos) * constant]])
+    return array([[0., (-1)*np.sin(angular_pos)*constant]])
 
 
 def hx(x):
@@ -20,27 +20,26 @@ def hx(x):
     would correspond to state x.
     """
 
-    return constant * sin(x[1])
+    return constant * cos(x[1])
 
 
 dt = 0.05
 rk = ExtendedKalmanFilter(dim_x=2, dim_z=1)
 
 #radar = RadarSim(dt, pos=0., vel=100., alt=1000.)
-detector = cc.PlanetDetector('/home/nvidia/OpenCV/Collaborative-Sensing/model/cascade.xml')
+detector = PlanetDetector('C:\\Users\\Phuc Dang\\Desktop\\Developer\\Collaborative-Sensing\\model\\cascade.xml')
 
 # make an imperfect starting guess
 #rk.x = array([radar.pos - 100, radar.vel + 100, radar.alt + 1000])
-rk.x = array([0, 0, 0])    #need better initial location
+rk.x = array([0, 0])    #need better initial location
 
-rk.F = eye(3) + array([[0, 1, 0],
-                       [0, 0, 0],
-                       [0, 0, 0]]) * dt
+rk.F = eye(2) + array([[0, 0],
+                       [1, 0]]) * dt
 
 range_std = 5.  # meters
 rk.R = np.diag([range_std ** 2])
 rk.Q[0:2, 0:2] = Q_discrete_white_noise(2, dt=dt, var=0.1)
-rk.Q[2, 2] = 0.1
+rk.Q[1, 1] = 0.1
 rk.P *= 50
 
 xs, track = [], []
