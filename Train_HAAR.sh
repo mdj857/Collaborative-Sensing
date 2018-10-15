f51cc76746d5c9c942297661e1d1884c0770e63d
Faisal Mahmood <faisalm1122@gmail.com>
2018-10-15 15:40:39 -0500

    w-works? with planetDetectorDriver???

--- Train_HAAR.sh
+++ Train_HAAR.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-opencv_traincascade -data model -bg bg.txt -numPos 30 -numNeg 798 -w 64 -h 64 -numStages 5 -vec positives.vec -featureType HAAR
+
+opencv_traincascade -data model -bg bg.txt -numPos 229 -numNeg 977 -w 50 -h 50 -numStages 2 -vec positives.vec -maxFalseAlarmRate 0.75 -featureType HAAR >> train_out.txt
+opencv_traincascade -data model_2 -bg bg2.txt -numPos 229 -numNeg 179 -w 50 -h 50 -numStages 2 -vec positives.vec -maxFalseAlarmRate 0.25 -featureType HAAR >> train2_out.txt
+opencv_traincascade -data model_better -bg bg.txt -numPos 229 -numNeg 977 -w 50 -h 50 -numStages 2 -vec positives.vec -maxFalseAlarmRate 0.75 -featureType HAAR >>trainb_out.txt
+opencv_traincascade -data model_2_better -bg bg2.txt -numPos 229 -numNeg 179 -w 50 -h 50 -numStages 2 -vec positives.vec -maxFalseAlarmRate 0.25 -featureType HAAR >>train2b.txt
--- src/car_detector.py
+++ src/car_detector.py
@@ -9,7 +9,10 @@ Created by Matthew Johnson 09/17/18
 import numpy as np
 import cv2
 
-rc_car_cascade10 = cv2.CascadeClassifier('/home/nvidia/OpenCV/Collaborative-Sensing/model/cascade.xml')
+
+rc_car_cascade10 = cv2.CascadeClassifier('/home/nvidia/OpenCV/Collaborative-Sensing/model_better/cascade.xml')
+#/home/nvidia/OpenCV/Collaborative-Sensing/old_models/model_4/cascade.xml')
+#rc_car_cascade10 = cv2.CascadeClassifier('/home/nvidia/OpenCV/Collaborative-Sensing/model/cascade.xml')
 #cap = cv2.VideoCapture(0)
 gst_str = ("v4l2src device=/dev/video{} ! "
                "video/x-raw, width=(int){}, height=(int){}, format=(string)RGB ! "
@@ -20,7 +23,7 @@ i=0
 font                   = cv2.FONT_HERSHEY_SIMPLEX
 bottomLeftCornerOfText = (10,500)
 fontScale              = 1
-fontColor              = (255,255,255)
+fontColor              = (128,128,128)
 lineType               = 2
 
 
@@ -28,29 +31,29 @@ lineType               = 2
 while 1:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    small = cv2.resize(gray, (0,0), fx=0.5, fy=0.5) 
 
     # add this
     # image, reject levels level weights.
-    rc_cars = rc_car_cascade10.detectMultiScale(gray, scaleFactor=50, minNeighbors=50)
+    rc_cars = rc_car_cascade10.detectMultiScale(small, scaleFactor=50, minNeighbors=50)
     
     # add this
     i=0
 
     for (x,y,w,h) in rc_cars:
 	i+=1
-        if(i>0 and i<17):
-          cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
-
-          cv2.putText(img,str(i), 
+        if(i>0 and i<50):
+          cv2.rectangle(small,(x,y),(x+w,y+h),(255,255,0),2)
+          cv2.putText(small,str(i), 
             (x,y), 
             font, 
             fontScale,
             fontColor,
             lineType) 
           roi_gray = gray[y:y+h, x:x+w]
-          roi_color = img[y:y+h, x:x+w]
+          roi_color = small[y:y+h, x:x+w]
 
-    cv2.imshow('img',img)
+    cv2.imshow('small',small)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
