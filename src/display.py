56131f775976ba1d5d366519299c63738bbbe2ef
manasreldin <nasreldin@utexas.edu>
2018-10-29 14:01:08 -0500

    spacing

--- src/display.py
+++ src/display.py
@@ -8,13 +8,12 @@ from planetDetector import *
 index = 0
 value = []
 
-class Desktop:
 
+class Desktop:
     def __init__(self, master):
         self.canvas = tk.Canvas(master, width=500, height=500)
         self.canvas.pack()
-        self.ball = self.canvas.create_oval(0,0,20,20, fill='red', tags='id')
-
+        self.ball = self.canvas.create_oval(0, 0, 20, 20, fill='red', tags='id')
 
 
 def update_label(root):
@@ -25,11 +24,12 @@ def update_label(root):
         print(index)
         radius.set("Radius: " + r)
         angle.set("Angle: " + a)
-        d.canvas.coords(d.ball, int(r)-10, int(a)-10, int(r)+10, int(a)+10)
+        d.canvas.coords(d.ball, int(r) - 10, int(a) - 10, int(r) + 10, int(a) + 10)
         root.after(1000, update)
         # line should be: R Theta
         for line in sys.stdin:
             value.append(line.split())
+
     update()
 
 
@@ -39,9 +39,9 @@ radius = tk.StringVar()
 angle = tk.StringVar()
 d = Desktop(root)
 root.title("Output Display")
-r = tk.Label(root, fg="green", textvariable = radius)
+r = tk.Label(root, fg="green", textvariable=radius)
 r.pack()
-a = tk.Label(root, fg="green", textvariable = angle)
+a = tk.Label(root, fg="green", textvariable=angle)
 a.pack()
 update_label(root)
 button = tk.Button(root, text='Stop', width=25, command=root.destroy)
