a287dbbf76a54361abc9f10c86902438e46c4d6f
manasreldin <nasreldin@utexas.edu>
2018-11-04 15:40:56 -0600

    check if opened

--- src/pyclient.py
+++ src/pyclient.py
@@ -6,6 +6,7 @@ OTHER_FIFO = "cli_recieve"
 with open(FIFO_FILENAME, "a") as f:
 	f.write("IPU!")
 with open(OTHER_FIFO, "r") as f1:
+        print("Opened Fifo")
         w = f1.read()
         print(f1)
         print(w)
