aab9001009f560b93533d657eb3301286db86c6d
TerryEunice <pcreipold@utexas.edu>
2018-11-04 15:34:35 -0600

    read and write pie-thon

--- src/pyclient.py
+++ src/pyclient.py
@@ -1,6 +1,11 @@
 import os
 import subprocess
-FIFO_FILENAME = "srv_transmit"
+FIFO_FILENAME = "cli_transmit"
+OTHER_FIFO = "cli_recieve"
 
 with open(FIFO_FILENAME, "a") as f:
-	f.write("IPU!") 
+	f.write("IPU!")
+with open(OTHER_FIFO, "r") as f1:
+        w = f1.read()
+        print(f1)
+        print(w)
