6905ad7068decdac9af9fc84a9ae3912a1689bde
phucdang18 <phucdang18@utexas.edu>
2018-11-05 14:46:49 -0600

    gui

--- src/display.py
+++ src/display.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 from time import sleep
-import sys
+from math import *
+import numpy as np
 
 index = 0
 value = []
+rad = 200
 
 class Desktop:
 
@@ -11,22 +13,32 @@ class Desktop:
         self.canvas = tk.Canvas(master, width=500, height=500)
         self.canvas.pack()
         self.ball = self.canvas.create_oval(0,0,20,20, fill='red', tags='id')
+        self.sun = self.canvas.create_oval(249-10, 249-10, 249 + 10, 249 + 10, fill='yellow', tags='id')
+        self.rect = self.canvas.create_rectangle(5,5,500,500)
 
 
+def convert(alpha):
+    alpha = alpha * np.pi / 180
+    x = 249 + rad * cos(alpha)
+    y = 249 + - (rad * sin(alpha))
+    return x, y
 
 def update_label(root):
     def update():
         global index
-        r, a = value[index]
+        alpha = int(value[index][0]) % (2 * np.pi)
+        r, a = convert(alpha)
         index = (index + 1) % size
         print(index)
-        radius.set("Radius: " + r)
-        angle.set("Angle: " + a)
+        radius.set("Radius: " + str(rad))
+        angle.set("Angle: " + str(alpha))
         d.canvas.coords(d.ball, int(r)-10, int(a)-10, int(r)+10, int(a)+10)
         root.after(1000, update)
         for line in sys.stdin:
             value.append(line.split())
+
     update()
+
 
 size = value.__len__()
 root = tk.Tk()
