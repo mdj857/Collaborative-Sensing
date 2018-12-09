6199c91ea86942ea59dc62830939a70975231557
manasreldin <nasreldin@utexas.edu>
2018-12-08 18:34:56 -0600

    moved CNN

---
+++ CNN/cnn_model.py
@@ -0,0 +1,78 @@
+from __future__ import print_function
+import keras
+from keras.layers import Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.models import Sequential
+import matplotlib.pylab as plt
+
+batch_size = 128
+epochs = 10
+
+# input image dimensions
+img_x, img_y = 128, 128
+
+# TODO: load data and split into train/test. 
+# X should be a uint8 array of size (num_samples, img_x, img_y, 3)
+# Y should be a uint8 aray of size (num_samples, 1, 2) (output vectors are of size 1x2)
+
+
+# reshape the data into a 4D tensor - (sample_number, x_img_size, y_img_size, num_channels)
+# because the MNIST is greyscale, we only three channels one for each RGB
+x_train = x_train.reshape(x_train.shape[0], img_x, img_y, 3)
+x_test = x_test.reshape(x_test.shape[0], img_x, img_y, 3)
+input_shape = (img_x, img_y, 3)
+
+# convert the data to the right type
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+
+model = Sequential()
+
+# first layer: 32 filters, 5x5 kernel, and unit strides 
+model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+# second layer: 64 filters, 5x5 kernel, and 2x2 strides for 'overlap'
+model.add(Conv2D(64, (5, 5), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# flatten and add two dense layers to reduce dimensionality to 2
+model.add(Flatten())
+model.add(Dense(1000, activation='relu'))
+model.add(Dense(2,))
+
+model.compile(loss=keras.losses.mean_squared_error,
+              optimizer=keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+
+class AccuracyHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.acc = []
+
+    def on_epoch_end(self, batch, logs={}):
+        self.acc.append(logs.get('acc'))
+
+history = AccuracyHistory()
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test),
+          callbacks=[history])
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+plt.plot(range(1, 11), history.acc)
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.show()
